8baab64d08523fb4201f6a14a3b87b65ad3cda86
Carlos <carlos.montoya5309@gmail.com>
2026-05-11 16:06:45 -0500

    feat: modified asyncio

--- src/async_repaso.py
+++ src/async_repaso.py
@@ -16,15 +16,36 @@ async def monitorear():
         print(f"monitoreando... {i}")
         await asyncio.sleep(0.5)
 
+async def leer_dato(numero):
+    print(f"Leyendo dato {numero}...")
+    await asyncio.sleep(1)
+    print(f"Dato {numero} leído!")
+    return numero
+
+async def procesar_datos():
+    while True:
+        print("Procesando datos...")
+        await asyncio.sleep(1)
+        print("Datos procesados!")
+
 #async def main():
 #    datos = await recibir_datos()
 #    await guardar_mongdb(datos)
 
+#async def main():
+#    await asyncio.gather(
+#        recibir_datos(),
+#        guardar_mongdb([1023, 987, 1005]),
+#        monitorear()
+#    )
+
 async def main():
     await asyncio.gather(
-        recibir_datos(),
-        guardar_mongdb([1023, 987, 1005]),
-        monitorear()
+        leer_dato(1),
+        leer_dato(2),
+        leer_dato(3),
+        procesar_datos()
     )
+
 
 asyncio.run(main())
