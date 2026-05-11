080a428f274af6b16b6a8daa3b416194e87794e2
Montoya <carlos.montoya5309@gmail.com>
2026-05-11 17:16:08 -0500

    feat: desafio completed

--- src/desafio_async.py
+++ src/desafio_async.py
@@ -1,7 +1,30 @@
 import asyncio
+import random
+
+numero = 0
+data = []
 
 async def leer_sensor():
     print("Leyendo sensor...")
-    await asyncio.sleep(0.1)  # Simula el tiempo de lectura
-    print("Sensor leído!")
-    return []
+    while True:
+        numero = random.randint(1, 10)
+        data.append(numero)
+        if len(data)==10:
+            promedio = sum(data)/len(data)
+            print(promedio)
+            data.clear()    
+        await asyncio.sleep(0.1)       
+    print(data)
+
+async def status():
+    while True:
+        print("sistema activo...")
+        await asyncio.sleep(2)
+    
+async def main():
+    await asyncio.gather(
+        leer_sensor(),
+        status()
+    )
+    
+asyncio.run(main())
